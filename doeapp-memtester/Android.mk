a7cc63c44afb0751816a59e6257985d4c841091b
Tinno <swrd@tinno.com>
2022-11-08 10:35:46 +0800

    Hawaii+ Lite push for android 12

---
+++ doeapp-memtester/Android.mk
@@ -0,0 +1,16 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/Android.mk
+
+LOCAL_SRC_FILES := \
+	memtester.c \
+	util.c \
+	tests.c
+
+LOCAL_MODULE := doeapp-memtester
+LOCAL_MODULE_TAGS := optional
+LOCAL_PROPRIETARY_MODULE := true
+LOCAL_MODULE_OWNER := mediatek
+
+include $(MTK_EXECUTABLE)
